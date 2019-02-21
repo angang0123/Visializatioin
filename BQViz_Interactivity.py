44c409d65bbca589f08129a23670c4bd5f545873
WestG3r <bashu_wnx@163.com>
2019-02-21 16:05:07 +0800

    Create BQViz_Interactivity.py

---
+++ BQViz_Interactivity.py
@@ -0,0 +1,4 @@
+'''
+Before running any of the examples in this notebook, you need to import the bqviz library, sample data, ipywidgets, 
+and some libraries we will use to generate random data.
+'''
